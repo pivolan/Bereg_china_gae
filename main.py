fc73e2d6ad1f44df9067557365e335d51c9e4eba
pivolan <pivolan@gmail.com>
2012-12-18 02:02:11 +0400

    добавлены 4 страницы без ссылок

--- main.py
+++ main.py
@@ -12,4 +12,22 @@ app = WSGIApplication(debug=True,
 @app.route('/')
 class UserList(BaseHandler):
 	def get(self):
-		return self.render_response('main.html')
+		return self.render_response('pages/main.html')
+
+
+@app.route('/proizvodstvennye-linii-iz-kitaya')
+class UserList(BaseHandler):
+	def get(self):
+		return self.render_response('pages/proizvodstvennye-linii-iz-kitaya.html')
+
+
+@app.route('/mini-zavody-iz-kitaya')
+class UserList(BaseHandler):
+	def get(self):
+		return self.render_response('pages/mini-zavody-iz-kitaya.html')
+
+
+@app.route('/oborudovanie-iz-kitaia')
+class UserList(BaseHandler):
+	def get(self):
+		return self.render_response('pages/oborudovanie-iz-kitaia.html')
