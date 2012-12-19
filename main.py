e957d44ffbb2d41aab5d14141709db29abfb48fa
pivolan <pivolan@gmail.com>
2012-12-20 02:47:21 +0400

    добавлен скрипт для обраотки таблицу с лишними параметрами. добавлена страница для ТПА

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 from libs.libs import WSGIApplication, BaseHandler
+from libs.BeautifulSoup import BeautifulStoneSoup
 
 config = {}
 config['webapp2_extras.sessions'] = {
@@ -85,3 +86,15 @@ class UserList(BaseHandler):
 class UserList(BaseHandler):
 	def get(self, model):
 		return self.render_response('tpa/%s.html' % model)
+
+@app.route('/table')
+class Table(BaseHandler):
+	def get(self):
+		rv = self.jinja2.render_template('pages/table.txt')
+		html = BeautifulStoneSoup(rv)
+		[span.replaceWith(span.renderContents()) for span in html.findAll('span')]
+		[span.replaceWith(span.renderContents()) for span in html.findAll('p')]
+		for tag in html.findAll(True):
+			tag.attrs = None
+		return self.response.write(html.renderContents())
+
