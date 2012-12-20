868f73f5dc0bf76dda185d0a9437f92cb26a23e3
pivolan <pivolan@gmail.com>
2012-12-20 04:24:48 +0400

    скрипт для преобразования всей страницы

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 from libs.libs import WSGIApplication, BaseHandler
-from libs.BeautifulSoup import BeautifulStoneSoup
+from libs.BeautifulSoup import BeautifulStoneSoup, Tag
+import re
 
 config = {}
 config['webapp2_extras.sessions'] = {
@@ -92,17 +93,41 @@ class UserList(BaseHandler):
 class Table(BaseHandler):
 	def post(self):
 		rv = self.request.POST['txt']
-		html = BeautifulStoneSoup(rv)
+		html = BeautifulStoneSoup(rv, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+		html.find('title').extract()
+		[span.extract() for span in html.findAll('a')]
+		for span in html.findAll('span', style=re.compile("text-decoration:underline")):
+			span.name = 'a'
+		for span in html.findAll('span', style=re.compile("font-weight:bold")):
+			span.name = 'strong'
+		for tag in html.findAll('p'):
+			for strong in tag.findAll('strong', text=re.compile("H2 ?.* H2")):
+				tag.name = 'h2'
+
+		#clean all span tags
 		[span.replaceWith(span.renderContents()) for span in html.findAll('span')]
-		[span.replaceWith(span.renderContents()) for span in html.findAll('p')]
+		#clean all p tags in table
+		[span.replaceWith(span.renderContents()) for span in html.find(['table', 'h2']).findAll(['p', 'strong'])]
+		#set header to table, and th tags instead of td for titles
 		for tr in html.findAll('tr'):
 			tr.find('td').name = 'th'
 		for td in html.find('tr').findAll('td'):
 			td.name = 'th'
 
+		thead = Tag(html, name='thead')
+		thead.insert(0, html.find('tr'))
+		html.find('table').insert(0, thead)
+		#erase all attrs for all tags
 		for tag in html.findAll(True):
-			tag.attrs = None
-		return self.render_response('pages/table.html', html=html.renderContents().decode('utf8'))
+			tag.attrs = []
+
+		for tag in html.findAll('img'):
+			tag.attrs = [('src', ''), ('alt', 'tpa')]
+
+		for tag in html.findAll('a'):
+			tag.attrs = [('href', '')]
+
+		return self.render_response('pages/table.html', html=html.body.renderContents().decode('utf8'))
 
 	def get(self):
 		return self.render_response('pages/table.html', html='')
